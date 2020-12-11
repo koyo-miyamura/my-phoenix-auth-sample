f339414b29c6414396fa3feeecf0b201b497eacc
koyo-miyamura <nexus9071@gmail.com>
2020-12-11 15:29:17 +0900

    保存時にパスワードをハッシュ化する

--- lib/myapp/accounts/user.ex
+++ lib/myapp/accounts/user.ex
@@ -7,13 +7,25 @@ defmodule Myapp.Accounts.User do
     field :name, :string
     field :password, :string
 
+    field :raw_password, :string, virtual: true
+    field :raw_password_confirmation, :string, virtual: true
+
     timestamps()
   end
 
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:name, :email, :password])
-    |> validate_required([:name, :email, :password])
+    |> cast(attrs, [:name, :email, :raw_password, :raw_password_confirmation])
+    |> validate_confirmation(:raw_password, message: "does not match password!")
+    |> encrypt_password
+    |> validate_required([:name, :email, :raw_password])
+  end
+
+  def encrypt_password(%Ecto.Changeset{changes: %{raw_password: raw_password}} = changeset) do
+    put_change(changeset, :password, Bcrypt.hash_pwd_salt(raw_password))
+  end
+  def encrypt_password(changeset) do
+    changeset
   end
 end
