6676a1a205441b98083f778bdbf98a30eddfc9c6
koyo-miyamura <nexus9071@gmail.com>
2020-12-11 15:28:52 +0900

    comeonin はもうインタフェースのみの提供になってった

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Myapp.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:comeonin, "~> 5.3"}
+      {:bcrypt_elixir, "~> 2.0"}
     ]
   end
 
